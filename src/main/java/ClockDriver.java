8766d6b43b58a7fe946ef7555525bf4830c66d78
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-20 23:25:39 -0700

    Update ClockDriver.java
    Completed clock driver instructions

--- src/main/java/ClockDriver.java
+++ src/main/java/ClockDriver.java
@@ -1,44 +1,54 @@
+package src.main.java;
 
-public class ClockDriver
-{
-    public static void main(String[] args){
-        //Implement tests for your ClockDisplay
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32 to 03:33
-        //  * Tick test for 03:09 to 03:10
-        //  * Tick test for 01:59 to 02:00
-        //  * Tick test for 09:59 to 10:00
-        //  * Tick test for 23:59 to 00:00
+public class ClockDriver {
+    public static void main(String[] args) {
+        // Testing ClockDisplay
+        System.out.println("Testing ClockDisplay:");
         
+        ClockDisplay test1 = new ClockDisplay();
+        System.out.println("\tEmpty Constructor Test - " + test1.getTime());
+
+        ClockDisplay test2 = new ClockDisplay(2, 45); // Remove seconds
+        System.out.println("\t2-Arg Constructor Test - " + test2.getTime());
+
+        test2.setTime(5, 15); // Remove seconds
+        System.out.println("\tSet Time Test - " + test2.getTime());
+
+        testTickTime(new ClockDisplay(3, 32), "03:32", "03:33");
+        testTickTime(new ClockDisplay(3, 9), "03:09", "03:10");
+        testTickTime(new ClockDisplay(1, 59), "01:59", "02:00");
+        testTickTime(new ClockDisplay(9, 59), "09:59", "10:00");
+        testTickTime(new ClockDisplay(23, 59), "23:59", "00:00");
+
+        // Testing ClockDisplaySeconds
+        System.out.println("\nTesting ClockDisplaySeconds:");
         
-        //Implement tests for your ClockDisplaySeconds
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32:59 to 03:33:00
-        //  * Tick test for 01:00:59 to 01:01:00
-        //  * Tick test for 01:59:59 to 02:00:00
-        //  * Tick test for 23:59:59 to 00:00:00
-        
-        /*CHALLENGE*/
-        //Implement tests for your ClockDisplay12Hour
-        //  * Test that you can do the basics
-        //      * Empty constructor & read time
-        //      * Constructor w/ time given & read time
-        //      * Set time
-        //      * Read time
-        //      * Tick time
-        //  * Tick test for 03:32PM to 03:33PM
-        //  * Tick test for 11:59PM to 12:00AM
-        //  * Tick test for 11:59AM to 12:00PM
-        //  * Tick test for 12:59PM to 01:00PM
+        ClockDisplaySeconds secondsTest1 = new ClockDisplaySeconds();
+        System.out.println("\tEmpty Constructor Test - " + secondsTest1.getTime());
+
+        ClockDisplaySeconds secondsTest2 = new ClockDisplaySeconds(2, 45, 30);
+        System.out.println("\t2-Arg Constructor Test - " + secondsTest2.getTime());
+
+        secondsTest2.setTime(5, 15, 45); // This now works
+        System.out.println("\tSet Time Test - " + secondsTest2.getTime());
+
+        testTickTimeSeconds(new ClockDisplaySeconds(3, 32, 59), "03:32:59", "03:33:00");
+        testTickTimeSeconds(new ClockDisplaySeconds(1, 0, 59), "01:00:59", "01:01:00");
+        testTickTimeSeconds(new ClockDisplaySeconds(1, 59, 59), "01:59:59", "02:00:00");
+        testTickTimeSeconds(new ClockDisplaySeconds(23, 59, 59), "23:59:59", "00:00:00");
+    }
+
+    private static void testTickTime(ClockDisplay clock, String before, String after) {
+        System.out.println("\tTick Test Before Increment - " + clock.getTime());
+        clock.timeTick();
+        System.out.println("\tTick Test After Increment - " + clock.getTime());
+        assert clock.getTime().equals(after) : "Expected " + after + " but got " + clock.getTime();
+    }
+
+    private static void testTickTimeSeconds(ClockDisplaySeconds clock, String before, String after) {
+        System.out.println("\tTick Test Before Increment - " + clock.getTime());
+        clock.timeTick();
+        System.out.println("\tTick Test After Increment - " + clock.getTime());
+        assert clock.getTime().equals(after) : "Expected " + after + " but got " + clock.getTime();
     }
 }
