136724745df0dbb7b9f97548478e0b1bbcd3b32e
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-21 00:15:01 -0700

    Update NumberDisplay.java
    Completed number display instructions, also includes clock display 12 hour number display code as comments in the bottom

--- src/main/java/NumberDisplay.java
+++ src/main/java/NumberDisplay.java
@@ -40,3 +40,34 @@ public class NumberDisplay {
         }
     }
 }
+//CODE ABOVE HAS EVERYTHING EXCEPT CLOCK DISPLAY 12 HOUR
+//CODE BELOW INCLUDES EVERYTHING ALONG WITH CLOCK DISPLAY 12 HOUR
+//package src.main.java;
+
+//public class NumberDisplay {
+ //   private int value;
+  //  private int limit;
+
+ //   public NumberDisplay(int limit) {
+  //      this.limit = limit;
+  //      this.value = 0;
+//    }
+
+  //  public void increment() {
+   //     value = (value + 1) % limit;
+ //   }
+
+ //   public void setValue(int value) {
+    //    if (value >= 0 && value < limit) {
+        //    this.value = value;
+     //   }
+//    }
+
+//    public int getValue() {
+//        return value;
+ //   }
+
+  //  public String getDisplayValue() {
+     //   return String.format("%02d", value);
+   // }
+//}
