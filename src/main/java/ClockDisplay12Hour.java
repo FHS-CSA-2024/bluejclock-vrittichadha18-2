bd6feef63e554518dc04afa3027f5e8d56f0ba87
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-21 00:02:06 -0700

    Update ClockDisplay12Hour.java
    Completed clock display 12 hour instructions

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -1,6 +1,33 @@
+package src.main.java;
 
-public class ClockDisplay12Hour
-{
-    //Extend ClockDisplay for a 12 hour clock. This means you need to track AM/PM
-    //Update all methods and parameters to accomodate this change
+public class ClockDisplay12Hour extends ClockDisplaySeconds {
+    private boolean isPM;
+
+    public ClockDisplay12Hour() {
+        super();
+        isPM = false; // Default to AM
+    }
+
+    public ClockDisplay12Hour(int hour, int minute, int second) {
+        super(hour % 12, minute, second); // Normalize hour to 12-hour format
+        isPM = hour >= 12;
+        updateDisplay();
+    }
+
+    @Override
+    public void setTime(int hour, int minute, int second) {
+        super.setTime(hour % 12, minute, second); // Normalize hour to 12-hour format
+        isPM = hour >= 12;
+    }
+
+    @Override
+    protected void updateDisplay() {
+        super.updateDisplay();
+        display += isPM ? " PM" : " AM"; // Append AM/PM
+    }
+
+    @Override
+    public String getTime() {
+        return super.getTime(); // Returns time including AM/PM
+    }
 }
