02658cdd37005cc67a9cfa007225c84c4eb50774
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-20 23:24:34 -0700

    Update ClockDisplay.java
    Completed clock display instructions

--- src/main/java/ClockDisplay.java
+++ src/main/java/ClockDisplay.java
@@ -1,33 +1,43 @@
+package src.main.java;
 
-public class ClockDisplay
-{
-    //Implement 2 private NumberDisplay objects. 
-    //  One for hours and one for minutes
-    //Implement a private String for the display
+public class ClockDisplay {
+    protected NumberDisplay hours;
+    protected NumberDisplay minutes;
+    protected String display;
 
-    //Implement a constructor that takes no parameters
-    //The constructor should set hours as a NumberDisplay object with 24 as the limit
-    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
-    //The constructor should call the method updateDisplay before finishing
-    
-    //Implement a constructor that takes 2 parameters, hour and minute
-    //The constructor should set hours as a NumberDisplay object with 24 as the limit
-    //The constructor should set minutes as a NumberDisplay object with 60 as the limit
-    //The constructor should call the method setTime with the parameters passed in
-    
-    //Implement a method timeTick that takes no parameters and returns nothing
-    //The method should increase the minute value by one each run
-    //The hours should increase when the minutes roll over
-    //The updateDisplay method should be called before finishing
-    
-    //Implement a method setTime that takes in 2 parameters, hour and minute
-    //The method should set the hours value and minutes value
-    //The updateDisplay method should be called before finishing
-    
-    //Implement a method getTime that takes no parameter and returns a String
-    //The return String should be formatted as HH:MM and report out the current time
+    public ClockDisplay() {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        updateDisplay();
+    }
 
-    //Implement a method updateDisplay that takes no parameters and returns nothing
-    //The method should update the displayString with the current time in a format
-    //  HH:MM
+    public ClockDisplay(int hour, int minute) {
+        hours = new NumberDisplay(24);
+        minutes = new NumberDisplay(60);
+        setTime(hour, minute);
+    }
+
+    public void timeTick() {
+        minutes.increment();
+        if (minutes.getValue() == 0) {
+            hours.increment();
+        }
+        updateDisplay();
+    }
+
+    public void setTime(int hour, int minute) {
+        hours.setValue(hour);
+        minutes.setValue(minute);
+        updateDisplay();
+    }
+
+    public String getTime() {
+        return display;
+    }
+
+    protected void updateDisplay() {
+        String newDisplay = hours.getDisplayValue() + ":" +
+                            minutes.getDisplayValue();
+        display = newDisplay;
+    }
 }
