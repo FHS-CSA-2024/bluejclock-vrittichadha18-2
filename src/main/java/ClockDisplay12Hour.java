b04c82e1db037fbfa78da1ba13995dd806194792
vrittichadha18 <chadhavritti18@gmail.com>
2024-09-21 17:23:42 -0700

    Update ClockDisplay12Hour.java
    Completed clock display 12 hour instructions, wrote as comments here because all the other classes also have included clock display 12 hour as comments below the original code

--- src/main/java/ClockDisplay12Hour.java
+++ src/main/java/ClockDisplay12Hour.java
@@ -1,33 +1,33 @@
-package src.main.java;
+//package src.main.java;
 
-public class ClockDisplay12Hour extends ClockDisplaySeconds {
-    private boolean isPM;
+//public class ClockDisplay12Hour extends ClockDisplaySeconds {
+   // private boolean isPM;
 
-    public ClockDisplay12Hour() {
-        super();
-        isPM = false; // Default to AM
-    }
+  //  public ClockDisplay12Hour() {
+   //     super();
+    //    isPM = false; // Default to AM
+  //  }
 
-    public ClockDisplay12Hour(int hour, int minute, int second) {
-        super(hour % 12, minute, second); // Normalize hour to 12-hour format
-        isPM = hour >= 12;
-        updateDisplay();
-    }
+   // public ClockDisplay12Hour(int hour, int minute, int second) {
+   //     super(hour % 12, minute, second); // Normalize hour to 12-hour format
+      //  isPM = hour >= 12;
+      //  updateDisplay();
+//    }
 
-    @Override
-    public void setTime(int hour, int minute, int second) {
-        super.setTime(hour % 12, minute, second); // Normalize hour to 12-hour format
-        isPM = hour >= 12;
-    }
+//    @Override
+//    public void setTime(int hour, int minute, int second) {
+ //       super.setTime(hour % 12, minute, second); // Normalize hour to 12-hour format
+  //      isPM = hour >= 12;
+  //  }
 
-    @Override
-    protected void updateDisplay() {
-        super.updateDisplay();
-        display += isPM ? " PM" : " AM"; // Append AM/PM
-    }
+  //  @Override
+   // protected void updateDisplay() {
+   //     super.updateDisplay();
+   //     display += isPM ? " PM" : " AM"; // Append AM/PM
+  //  }
 
-    @Override
-    public String getTime() {
-        return super.getTime(); // Returns time including AM/PM
-    }
-}
+//    @Override
+  //  public String getTime() {
+      //  return super.getTime(); // Returns time including AM/PM
+  //  }
+// }
